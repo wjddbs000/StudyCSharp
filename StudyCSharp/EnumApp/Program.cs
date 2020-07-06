6e2e883fd4e5f7d5bc8100077e94798d86fbd672
ktk <vodjaos@gmail.com>
2020-07-06 09:50:18 +0900

    구냥

---
+++ StudyCSharp/EnumApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumApp
+{
+    class Program
+    {
+        enum DialogResult
+        {
+            YES,
+            NO,
+            CANCEL,
+            CONFIRM,
+            OK
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DialogResult.OK);
+            Console.WriteLine((int)DialogResult.OK);
+        }
+    }
+}
